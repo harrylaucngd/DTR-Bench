2b4b7c2a32a7e9cdcbb561a401f97c7065629be9
Zhiyao Luo <luozhiyao933@126.com>
2024-07-18 14:42:29 +0100

    commit sweep id

--- DTRBench/run_RL/online_discrete_search.py
+++ DTRBench/run_RL/online_discrete_search.py
@@ -58,7 +58,7 @@ def parse_args():
                              "Wandb sweep won't work correctly if this is not changed!")
 
     parser.add_argument("--device", type=str, default="cuda" if torch.cuda.is_available() else "cpu")
-    parser.add_argument("--role", type=str, default="sweep", choices=["sweep", "agent", "run_single"])
+    parser.add_argument("--role", type=str, default="agent", choices=["sweep", "agent", "run_single"])
     args = parser.parse_known_args()[0]
 
     return args
